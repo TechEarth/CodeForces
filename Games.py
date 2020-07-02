891037d9b7a79ac08613aeb60583baa59a26fc6e
Devesh Bajpai <devesh.bajpai19@gmail.com>
2020-07-02 18:41:26 +0530

    brute force

--- Games.py
+++ Games.py
@@ -4,11 +4,11 @@ __author__ = 'Devesh Bajpai'
 https://codeforces.com/problemset/problem/268/A
 
 Solution: There are few things to unravel in this problem. Each team would play two matches, one at its home ground and
-other at away ground. Hence, we need to track it via two loops. We would run into the case where we can the same team
+other at away ground. Hence, we need to track it via two loops. We would run into the case where we can have the same team
 index for home and away, hence we skip that case. Now for a match, we capture the home team's home jersey color (number)
 and the away team away jersey color (number). If those are same, the home team would be subjected to wear its away
 jersey and that is what we need to count for the result. Hence, we keep a counter for home team's away jersey wearing
-occurrences. We update these occurrences and return that answer finally.   
+occurrences. We count these occurrences and return that answer finally.   
 '''
 
 
@@ -18,6 +18,7 @@ def solve(teamsJerseys):
     for homeTeamIdx in xrange(0, len(teamsJerseys)):
         for awayTeamIdx in xrange(0, len(teamsJerseys)):
 
+            # team won't play with itself, so skip
             if homeTeamIdx == awayTeamIdx:
                 continue
 
@@ -36,7 +37,7 @@ if __name__ == "__main__":
     n = int(raw_input())
 
     teamsJerseys = list()
-    for _n in xrange(0,n):
+    for _ in xrange(0, n):
         h, a = map(int, raw_input().split(" "))
         teamsJerseys.append((h, a))
 
