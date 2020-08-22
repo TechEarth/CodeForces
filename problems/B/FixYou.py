a895fe75c86926c7a0c35b7269c02cbfc7da3c3d
Devesh Bajpai <devesh.bajpai19@gmail.com>
2020-08-22 19:16:36 +0530

    implementation

--- problems/B/FixYou.py
+++ problems/B/FixYou.py
@@ -5,7 +5,7 @@ https://codeforces.com/problemset/problem/1391/B
 
 Solution: All cells eventually drain the luggage to either last row or last column so as to reach the 
 destination counter. Therefore, any 'D' in the last row and any 'R' in the last column will force the
-luggage go out of the grid thereby not reaching the counter. We need to count these occurences and return
+luggage go out of the grid thereby not reaching the counter. We need to count these occurrences and return
 it as the answer. 
 '''
 
