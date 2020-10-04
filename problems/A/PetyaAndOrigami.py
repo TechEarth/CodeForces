8c8a93927d40ae6f92d6cb78af208c50bbe68db4
Devesh Bajpai <devesh.bajpai19@gmail.com>
2020-10-04 19:23:53 +0530

    greedy

--- problems/A/PetyaAndOrigami.py
+++ problems/A/PetyaAndOrigami.py
@@ -3,7 +3,8 @@ __author__ = 'Devesh Bajpai'
 '''
 https://codeforces.com/problemset/problem/1080/A
 
-Solution: TODO
+Solution: Calculate the number of red, green and blue papers needed. Since each book provides k sheets, we divide 
+each color's paper needed by k to get the number of books. To take care of remainder as well we do ceil division.  
 '''
 
 
